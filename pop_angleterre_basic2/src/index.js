2e93d7af81d7aa598034d120f05c37c441f93dda
Pierre BIDAULT <debiantarte@minet.net>
2019-05-26 18:41:52 +0200

    utilisation de Promise pour charger les deux jeux de données en même temps

--- pop_angleterre_basic2/src/index.js
+++ pop_angleterre_basic2/src/index.js
@@ -48,129 +48,137 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // d3
     console.log("loading data...");
-    d3.json('data/EN.json').then(function (data) {
-        d3.csv('data/EN_wealth.csv').then(function (data2) {
-            // calcul des min/max des données pour faire des échelles
-            // *_extent[0] = minimum, *_extent[1] = maximum
-            console.log("loaded");
-            var population_extent = d3.extent(data, function (d) {
-                return d.population;
-            });
-
-            var longitude_extent = d3.extent(data, function (d) {
-                if (d !== undefined) { // au cas où on tombe sur une ligne malformée (?)
-                    return d.longitude;
-                }
-            });
-
-            var latitude_extent = d3.extent(data, function (d) {
-                if (d !== undefined) {
-                    return d.latitude;
-                }
-            });
-
-            var population_extent_bis = d3.extent(data2, function (d) {
-                return d.GCP_MER05;
-            });
-
-            var longitude_extent_bis = d3.extent(data2, function (d) {
-                if (d !== undefined) { // au cas où on tombe sur une ligne malformée (?)
-                    return d.X;
-                }
-            });
-
-            var latitude_extent_bis = d3.extent(data2, function (d) {
-                if (d !== undefined) {
-                    return d.Y;
-                }
-            });
-
-            // création des échelles des champs qu'on va utiliser
-            // ici : échelle linéaire
-            // 0 -> 0, population maximum -> 10
-            var population_scale = d3.scaleLinear()
-                .domain([0, population_extent[1]])
-                .range([0, 10]);
-            var longitude_scale = d3.scaleLinear()
-                .domain([longitude_extent[0], longitude_extent[1]])
-                .range([20, 0]);
-            var latitude_scale = d3.scaleLinear()
-                .domain([latitude_extent[0], latitude_extent[1]])
-                .range([30, 0]);
-
-            var population_scale_bis = d3.scaleLinear()
-                .domain([0, population_extent_bis[1]])
-                .range([0, 10]);
-            var longitude_scale_bis = d3.scaleLinear()
-                .domain([longitude_extent_bis[0], longitude_extent_bis[1]])
-                .range([20, 0]);
-            var latitude_scale_bis = d3.scaleLinear()
-                .domain([latitude_extent_bis[0], latitude_extent_bis[1]])
-                .range([30, 0]);
-            
-            console.log("fini calculs");
-            // Lier les objets a-frame et les données :
-            var u = d3.select('a-scene') // sélection de la scène a-frame
-                .selectAll('a-box.bar') // sélection des cubes
-                .data(data) // qu'on lie aux données
-
-            var v = d3.select('a-scene') // sélection de la scène a-frame
-                .selectAll('a-box.bar2') // sélection des cubes
-                .data(data2) // qu'on lie aux données
-            
-            // Mise à jour des objets
-            u.enter() // pour toutes les nouvelles données
-                .append('a-box') // créer un cube s'il n'y en a pas assez
-                .classed('bar bar1', true) // lui donner la classe css "bar"
-            // on fait correspondre les champs des données à des paramètres visibles
-                .attr("height", function (d) { // hauteur du cube -> population
-                    return population_scale(d.population);
-                })
-                .attr("position", function (d) {
-
-                    y = population_scale(d.population) / 2; // position y -> population / 2
-                    x = longitude_scale(d.longitude); // position x -> latitude
-                    z = latitude_scale(d.latitude); // position z -> longitude
-                    // ici la projection est très simple : x -> latitude, z -> longitude
-                    // c'est une projection cylindrique (?), très déformée aux pôles
-                    // mais vu qu'on est sur une petite échelle, la déformation devrait être négligeable
-                    return x + " " + y + " " + z;
-                })
-                .attr("color", function () {
-                    return "#CFD" + (Math.floor(Math.random() * 500) + 200)
-                })
-                .attr("scale", "0.3 1 0.3")
-
-            // suppression des objets en trop
-            u.exit() // pour tous les objets en trop
-                .remove();// on vire
-
-            // Mise à jour des objets
-            v.enter() // pour toutes les nouvelles données
-                .append('a-box') // créer un cube s'il n'y en a pas assez
-                .classed('bar bar2', true) // lui donner la classe css "bar"
-            // on fait correspondre les champs des données à des paramètres visibles
-                .attr("height", function (d) { // hauteur du cube -> population
-                    return population_scale_bis(d.population);
-                })
-                .attr("position", function (d) {
-
-                    y = population_scale_bis(d.population) / 2; // position y -> population / 2
-                    x = longitude_scale_bis(d.longitude); // position x -> latitude
-                    z = latitude_scale_bis(d.latitude); // position z -> longitude
-                    // ici la projection est très simple : x -> latitude, z -> longitude
-                    // c'est une projection cylindrique (?), très déformée aux pôles
-                    // mais vu qu'on est sur une petite échelle, la déformation devrait être négligeable
-                    return x + " " + y + " " + z;
-                })
-                .attr("color", function () {
-                    return "#CFD" + (Math.floor(Math.random() * 500) + 200)
-                })
-                .attr("scale", "0.3 1 0.3")
-
-            // suppression des objets en trop
-            v.exit() // pour tous les objets en trop
-                .remove();// on vire
+    Promise.all([ //Dans mes souvenirs, 'Promise' est dangereux à utiliser mais permet de 'promettre' au compilateur qu'il aura accès à des données pendant ses calculs
+        d3.json('data/EN.json'),
+        d3.json('data/EN_wealth.json'),
+    ]).then(function (data) { //data[0] contient EN.json, data[1] contient EN_wealth.json
+        
+        // calcul des min/max des données pour faire des échelles
+        // *_extent[0] = minimum, *_extent[1] = maximum
+        console.log("loaded");
+        var population_extent = d3.extent(data[0], function (d) {
+            return d.population;
         });
+        
+        var longitude_extent = d3.extent(data[0], function (d) {
+            if (d !== undefined) { // au cas où on tombe sur une ligne malformée (?)
+                return d.longitude;
+            }
+        });
+        
+        var latitude_extent = d3.extent(data[0], function (d) {
+            if (d !== undefined) {
+                return d.latitude;
+            }
+        });
+
+        
+        
+        // création des échelles des champs qu'on va utiliser
+        // ici : échelle linéaire
+        // 0 -> 0, population maximum -> 10
+        var population_scale = d3.scaleLinear()
+            .domain([0, population_extent[1]])
+            .range([0, 10]);
+        var longitude_scale = d3.scaleLinear()
+            .domain([longitude_extent[0], longitude_extent[1]])
+            .range([20, 0]);
+        var latitude_scale = d3.scaleLinear()
+            .domain([latitude_extent[0], latitude_extent[1]])
+            .range([30, 0]);
+            
+        console.log("fini calculs");
+        // Lier les objets a-frame et les données :
+        var u = d3.select('a-scene') // sélection de la scène a-frame
+            .selectAll('a-box.bar') // sélection des cubes
+            .data(data[0]) // qu'on lie aux données
+            
+        // Mise à jour des objets
+        u.enter() // pour toutes les nouvelles données
+            .append('a-box') // créer un cube s'il n'y en a pas assez
+            .classed('bar bar1', true) // lui donner la classe css "bar"
+        // on fait correspondre les champs des données à des paramètres visibles
+            .attr("height", function (d) { // hauteur du cube -> population
+                return population_scale(d.population);
+            })
+            .attr("position", function (d) {
+                
+                y = population_scale(d.population) / 2; // position y -> population / 2
+                x = longitude_scale(d.longitude); // position x -> latitude
+                z = latitude_scale(d.latitude); // position z -> longitude
+                // ici la projection est très simple : x -> latitude, z -> longitude
+                // c'est une projection cylindrique (?), très déformée aux pôles
+                // mais vu qu'on est sur une petite échelle, la déformation devrait être négligeable
+                return x + " " + y + " " + z;
+            })
+            .attr("color", function () {
+                return "#0000FF" //"#CFD" + (Math.floor(Math.random() * 500) + 200)
+            })
+            .attr("scale", "0.3 1 0.3")
+
+        // suppression des objets en trop
+        u.exit() // pour tous les objets en trop
+            .remove();// on vire
+        
+        
+    //});
+
+    //d3.json('data/EN_wealth.json').then(function (data2) {
+        var population_extent_bis = d3.extent(data[1], function (d) {
+            return d.pop05;
+        });
+    
+        var longitude_extent_bis = d3.extent(data[1], function (d) {
+            if (d !== undefined) { // au cas où on tombe sur une ligne malformée (?)
+                return d.longitude;
+            }
+        });
+    
+        var latitude_extent_bis = d3.extent(data[1], function (d) {
+            if (d !== undefined) {
+                return d.latitude;
+            }
+        });
+
+        var population_scale_bis = d3.scaleLinear()
+            .domain([0, population_extent_bis[1]])
+            .range([0, 10]);
+        var longitude_scale_bis = d3.scaleLinear()
+            .domain([longitude_extent_bis[0], longitude_extent_bis[1]])
+            .range([20, 0]);
+        var latitude_scale_bis = d3.scaleLinear()
+            .domain([latitude_extent_bis[0], latitude_extent_bis[1]])
+            .range([30, 0]);
+
+        var v = d3.select('a-scene') // sélection de la scène a-frame
+            .selectAll('a-box.bar2') // sélection des cubes
+            .data(data[1]) // qu'on lie aux données
+
+        // Mise à jour des objets
+        v.enter() // pour toutes les nouvelles données
+            .append('a-box') // créer un cube s'il n'y en a pas assez
+            .classed('bar bar2', true) // lui donner la classe css "bar"
+        // on fait correspondre les champs des données à des paramètres visibles
+            .attr("height", function (d) { // hauteur du cube -> population
+                return population_scale_bis(d.population);
+            })
+            .attr("position", function (d) {
+                
+                y = population_scale_bis(d.population) / 2; // position y -> population / 2
+                x = longitude_scale_bis(d.longitude); // position x -> latitude
+                z = latitude_scale_bis(d.latitude); // position z -> longitude
+                // ici la projection est très simple : x -> latitude, z -> longitude
+                // c'est une projection cylindrique (?), très déformée aux pôles
+                // mais vu qu'on est sur une petite échelle, la déformation devrait être négligeable
+                return x + " " + y + " " + z;
+            })
+            .attr("color", function () {
+                return "#FF0000"
+            })
+            .attr("scale", "0.3 1 0.3")
+        
+        // suppression des objets en trop
+        v.exit() // pour tous les objets en trop
+            .remove();// on vire
     });
 });
