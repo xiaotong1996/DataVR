95ac1427d2af35a99f0d19409ccd3e090b7b195c
Etienne ANDRE <eti.andre@gmail.com>
2019-05-27 15:00:35 +0200

    merge multi-data :) :) :) :)

--- pop_angleterre_basic2/src/index.js
+++ pop_angleterre_basic2/src/index.js
@@ -7,25 +7,31 @@ AFRAME.registerComponent('pop-angleterre', {
     init: function () {
         // d3
         console.log("loading data...");
-        d3.json('data/EN.json').then(function (data) {
+        Promise.all([ //Dans mes souvenirs, 'Promise' est dangereux à utiliser mais permet de 'promettre' au compilateur qu'il aura accès à des données pendant ses calculs
+            d3.json('data/EN.json'),
+            d3.json('data/EN_wealth.json'),
+        ]).then(function (data) { //data[0] contient EN.json, data[1] contient EN_wealth.json
+
             // calcul des min/max des données pour faire des échelles
             // *_extent[0] = minimum, *_extent[1] = maximum
             console.log("loaded");
-            var population_extent = d3.extent(data, function (d) {
+            var population_extent = d3.extent(data[0], function (d) {
                 return d.population;
             });
 
-            var longitude_extent = d3.extent(data, function (d) {
+            var longitude_extent = d3.extent(data[0], function (d) {
                 if (d !== undefined) { // au cas où on tombe sur une ligne malformée (?)
                     return d.longitude;
                 }
             });
 
-            var latitude_extent = d3.extent(data, function (d) {
+            var latitude_extent = d3.extent(data[0], function (d) {
                 if (d !== undefined) {
                     return d.latitude;
                 }
             });
+
+
 
             // création des échelles des champs qu'on va utiliser
             // ici : échelle linéaire
@@ -35,7 +41,7 @@ AFRAME.registerComponent('pop-angleterre', {
                 .range([0, 10]);
             var longitude_scale = d3.scaleLinear()
                 .domain([longitude_extent[0], longitude_extent[1]])
-                .range([0, 20]);
+                .range([20, 0]);
             var latitude_scale = d3.scaleLinear()
                 .domain([latitude_extent[0], latitude_extent[1]])
                 .range([30, 0]);
@@ -43,8 +49,9 @@ AFRAME.registerComponent('pop-angleterre', {
             console.log("fini calculs");
             // Lier les objets a-frame et les données :
             var u = d3.select('a-scene') // sélection de la scène a-frame
+                .select('a-entity.set1') // séléction de la a-entity correspondante au bon set
                 .selectAll('a-box.bar') // sélection des cubes
-                .data(data) // qu'on lie aux données
+                .data(data[0]) // qu'on lie aux données
 
             // Mise à jour des objets
             u.enter() // pour toutes les nouvelles données
@@ -65,12 +72,74 @@ AFRAME.registerComponent('pop-angleterre', {
                     return x + " " + y + " " + z;
                 })
                 .attr("color", function () {
-                    return "#CFD" + (Math.floor(Math.random() * 500) + 200)
+                    return "#0000FF" //"#CFD" + (Math.floor(Math.random() * 500) + 200)
                 })
                 .attr("scale", "0.3 1 0.3")
 
             // suppression des objets en trop
             u.exit() // pour tous les objets en trop
+                .remove();// on vire
+
+
+            //});
+
+            //d3.json('data/EN_wealth.json').then(function (data2) {
+            var population_extent_bis = d3.extent(data[1], function (d) {
+                return d.GCP_MER05;
+            });
+
+            var longitude_extent_bis = d3.extent(data[1], function (d) {
+                if (d !== undefined) { // au cas où on tombe sur une ligne malformée (?)
+                    return d.longitude;
+                }
+            });
+
+            var latitude_extent_bis = d3.extent(data[1], function (d) {
+                if (d !== undefined) {
+                    return d.latitude;
+                }
+            });
+
+            var population_scale_bis = d3.scaleLinear()
+                .domain([0, population_extent_bis[1]])
+                .range([0, 10]);
+            var longitude_scale_bis = d3.scaleLinear()
+                .domain([longitude_extent_bis[0], longitude_extent_bis[1]])
+                .range([20, 0]);
+            var latitude_scale_bis = d3.scaleLinear()
+                .domain([latitude_extent_bis[0], latitude_extent_bis[1]])
+                .range([30, 0]);
+
+            var v = d3.select('a-scene') // sélection de la scène a-frame
+                .select('a-entity.set2')
+                .selectAll('a-box.bar') // sélection des cubes
+                .data(data[1]) // qu'on lie aux données
+
+            // Mise à jour des objets
+            v.enter() // pour toutes les nouvelles données
+                .append('a-box') // créer un cube s'il n'y en a pas assez
+                .classed('bar', true) // lui donner la classe css "bar"
+                // on fait correspondre les champs des données à des paramètres visibles
+                .attr("height", function (d) { // hauteur du cube -> GCP en 2005
+                    return population_scale_bis(d.GCP_MER05);
+                })
+                .attr("position", function (d) {
+
+                    y = population_scale_bis(d.GCP_MER05) / 2; // position y -> GCP / 2
+                    x = longitude_scale_bis(d.longitude); // position x -> latitude
+                    z = latitude_scale_bis(d.latitude); // position z -> longitude
+                    // ici la projection est très simple : x -> latitude, z -> longitude
+                    // c'est une projection cylindrique (?), très déformée aux pôles
+                    // mais vu qu'on est sur une petite échelle, la déformation devrait être négligeable
+                    return x + " " + y + " " + z;
+                })
+                .attr("color", function () {
+                    return "#FF0000"
+                })
+                .attr("scale", "0.3 1 0.3")
+
+            // suppression des objets en trop
+            v.exit() // pour tous les objets en trop
                 .remove();// on vire
         });
     }
@@ -79,7 +148,7 @@ AFRAME.registerComponent('pop-angleterre', {
 AFRAME.registerComponent('ukmap', {
     schema: { canvas: { type: 'selector' } },
     init: function () {
-        
+
         var canvas = this.canvas = this.data.canvas;
         var ctx = this.ctx = canvas.getContext('2d');
         // fill red for debug
@@ -102,10 +171,10 @@ AFRAME.registerComponent('ukmap', {
 
             ctx.lineWidth = 0.5;
             ctx.strokeStyle = '#aaa';
-            
+
             // fix me !!!
             ctx.beginPath();
-            geoGenerator({type: 'FeatureCollection', features: data.features})
+            geoGenerator({ type: 'FeatureCollection', features: data.features })
             ctx.stroke();
 
         });
